75489af19ad472eb9e2aef7624876b592962b9cf
菅原誠一 <poipoipoi33378@gmail.com>
2018-05-06 21:29:48 +0900

    引数渡しをやめて直接メンバー変数を参照する様に変更

--- triangle.rb
+++ triangle.rb
@@ -3,20 +3,20 @@ class Triangle
     array = [a,b,c]
     @length = array.sort
   end
-  def name
 
-    if check_triganle(@length) then
+  def name
+    if check_triganle() then
       '三角形でない'
-    elsif check_equilateral_triangle(@length) then
+    elsif check_equilateral_triangle() then
       '正三角形'
-    elsif check_isosceles_triangle(@length) then
-      if check_right_triangle(@length)
+    elsif check_isosceles_triangle() then
+      if check_right_triangle()
         '直角二等辺三角形'
       else
         '二等辺三角形'
       end
     else
-      if check_right_triangle(@length)
+      if check_right_triangle()
         '直角三角形'
       else
         '不等辺三角形'
@@ -24,20 +24,20 @@ class Triangle
     end
   end
   private
-  def check_right_triangle(length)
-    x = (length[0]**2 + length[1]**2)**(1/2.0)
-    (x == length[2])
+  def check_right_triangle
+    x = (@length[0]**2 + @length[1]**2)**(1/2.0)
+    (x == @length[2])
   end
 
-  def check_equilateral_triangle(length)
-    length[0]==length[1] and length[1]==length[2] and length[2]==length[0]
+  def check_equilateral_triangle
+    @length[0]==@length[1] and @length[1]==@length[2] and @length[2]==@length[0]
   end
 
-  def check_isosceles_triangle(length)
-    length[0]==length[1] or length[1]==length[2] or length[2]==length[0]
+  def check_isosceles_triangle
+    @length[0]==@length[1] or @length[1]==@length[2] or @length[2]==@length[0]
   end
 
-  def check_triganle(length)
-    length[2] >= (length[0] + length[1])
+  def check_triganle
+    @length[2] >= (@length[0] + @length[1])
   end
 end
