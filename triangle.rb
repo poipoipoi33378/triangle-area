4c2e9bc6d7a57726960120845bcde49916e3b1ff
菅原誠一 <poipoipoi33378@gmail.com>
2018-05-06 21:24:56 +0900

    使っていないクラス変数削除

--- triangle.rb
+++ triangle.rb
@@ -1,25 +1,22 @@
 class Triangle
   def initialize(a:, b: ,c:)
-    @a = a
-    @b = b
-    @c = c
+    array = [a,b,c]
+    @length = array.sort
   end
   def name
-    array = [@a,@b,@c]
-    length = array.sort
 
-    if length[2] >= (length[0] + length[1]) then
+    if check_triganle(@length) then
       '三角形でない'
-    elsif @a==@b and @b==@c and @a==@c then
+    elsif check_equilateral_triangle(@length) then
       '正三角形'
-    elsif @a==@b or @b==@c or @c==@a then
-      if (right_triangle(array))
+    elsif check_isosceles_triangle(@length) then
+      if check_right_triangle(@length)
         '直角二等辺三角形'
       else
         '二等辺三角形'
       end
     else
-      if (right_triangle(array))
+      if check_right_triangle(@length)
         '直角三角形'
       else
         '不等辺三角形'
@@ -27,13 +24,20 @@ class Triangle
     end
   end
   private
-  def right_triangle(array)
-    length = array.sort!
+  def check_right_triangle(length)
     x = (length[0]**2 + length[1]**2)**(1/2.0)
-    if (x == length[2])
-      true
-    else
-      false
-    end
+    (x == length[2])
+  end
+
+  def check_equilateral_triangle(length)
+    length[0]==length[1] and length[1]==length[2] and length[2]==length[0]
+  end
+
+  def check_isosceles_triangle(length)
+    length[0]==length[1] or length[1]==length[2] or length[2]==length[0]
+  end
+
+  def check_triganle(length)
+    length[2] >= (length[0] + length[1])
   end
 end
